0608f50796d76db49260b8f632f62e8506c4e0b5
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-30 10:16:13 -0600

    add faraday connection and response endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
   def index
-    @stations = Station.where(params[:q])
+    zipcode = params[:zipcode]
+
+    @conn = Faraday.new(url: 'https://developer.nrel.gov/api/') do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['dev_network_key']
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = @conn.get("/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2")
+
+    @stations = JSON.parse(response.body, symbolize_names: true)
   end
 end
